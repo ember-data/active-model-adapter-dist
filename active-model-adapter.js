b2bcf68262a597dcbdc65f9e3497ca67d5484bbd
Tomster Automated Release <noreply@emberjs.com>
2015-06-14 17:23:22 +0000

    release 0.1.13

--- active-model-adapter.js
+++ active-model-adapter.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   0.1.12
+ * @version   0.1.13
  */
 
 (function() {
